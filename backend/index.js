f316d13829005c018a5e84dc9c6c30a91e92eeae
saikat7890 <124331158+saikat7890@users.noreply.github.com>
2024-11-18 22:53:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,7 @@ const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
 const io = require("socket.io")(server, {
     pingTimeOut: 60000,
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://real-time-chat-app-frontend-r5qy.onrender.com",
     },
 });
 
