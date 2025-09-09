b31874d9cdfaa78fd1690c1264d46ec8abc6f6d7
saikat7890 <saikathazra9732@gmail.com>
2025-09-10 01:15:28 +0530

    Env name changed- index.js & db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.LOCAL_CONN_STR);
+        const conn = await mongoose.connect(process.env.CONN_STR);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 //middlewares
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL,
   credentials: true
 }));
 
